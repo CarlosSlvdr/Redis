2ada8339e42d6398c4a9fe146b0e56b37c19aaa6
JC <jcsalvador2105@gmail.com>
2025-08-25 16:12:40 +0800

    Delete Event Listener

--- src/test/java/com/vinsguru/redission/test/Lec05EventListenerTest.java
+++ src/test/java/com/vinsguru/redission/test/Lec05EventListenerTest.java
@@ -1,6 +1,7 @@
 package com.vinsguru.redission.test;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.DeletedObjectListener;
 import org.redisson.api.ExpiredObjectListener;
 import org.redisson.api.RBucketReactive;
 import org.redisson.client.codec.StringCodec;
@@ -31,7 +32,28 @@ public class Lec05EventListenerTest extends BaseTest{
 
         //extend
         sleep(11000);
+    }
 
+    @Test
+    public void deletedEventTest() {
+        RBucketReactive<Object> bucket = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
+        Mono<Void> set = bucket.set("sam");
+        Mono<Void> get = bucket.get()
+                .doOnNext(System.out::println)
+                .then();
+
+        Mono<Void> event = bucket.addListener(new DeletedObjectListener() {
+            @Override
+            public void onDeleted(String s) {
+                System.out.println("Deleted : " + s);
+            }
+        }).then();
+
+        StepVerifier.create(set.concatWith(get).concatWith(event))
+                .verifyComplete();
+
+        //extend
+        sleep(60000);
     }
 
 }
